37b5a4f86bc114a733237329cb054cc5fa66d30e
unknown <gustavolscmidt13@gmail.com>
2024-02-27 19:49:52 -0300

    OT-9

--- OT1/js/site.js
+++ OT1/js/site.js
@@ -37,6 +37,21 @@ function validaFaleConosco() {
         return false;
 
     }
+    var nome = document.frmfaleconosco.txtnome.value;
+    var expRegNome = new RegExp("^[A-zÀ-ü]{3,}([ ]{1}[A-zÀ-ü]{2,})$");
+    if (!expRegNome.test(nome)) {
+        alert("Preencha o campo Nome corretamente.");
+        document.frmfaleconosco.txtnome.focus();
+        return false;
+    }
+
+    var fone = document.frmfaleconosco.txtfone.value;
+    var expRegFone = new RegExp("^[(]{1}[1-9]{2}[)]{1}[0-9]{4,5}[-]{1}[0-9]{4}$");
+    if (!expRegFone.test(fone)) {
+        alert("Preencha o campo Telefone corretamente.");
+        document.frmfaleconosco.txtfone.focus();
+        return false;
+    }
 
     return true;
 }
